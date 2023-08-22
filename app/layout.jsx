5eb2372d3f5fd0af1e9c4e002a4a55d53f2d2dca
TheAimHero <vsghodekar1@gmail.com>
2023-08-22 23:39:26 +0530

    [feat] Connect page initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -31,11 +31,12 @@ export default function RootLayout(props) {
       <Link href={href} key={id}>
         <div
           className={`${background} flex h-[100px] w-[100px] items-end border-[2px] border-solid border-black 
-                    sm:h-52 sm:w-52`}
+                      sm:h-52 sm:w-52`}
         >
           <span
-            className={`mb-2 w-full bg-slate-200/75 text-center font-ranga text-xs hover:bg-slate-700/75 
-                      hover:text-white sm:text-lg`}
+            className={`mb-2 w-full bg-slate-200/75 text-center font-ranga text-xs 
+                        hover:bg-slate-700/75 hover:text-white 
+                        sm:text-lg`}
           >
             {text}
           </span>
@@ -49,17 +50,21 @@ export default function RootLayout(props) {
       <body
         className={`flex min-h-screen items-center justify-center bg-slate-500 font-roboto`}
       >
-        <div className='mx-auto flex w-screen flex-col items-center pb-4 pt-4 sm:w-[1000px]'>
-          <div className='flex w-full flex-col items-center justify-between bg-slate-100 pb-5 pt-5 sm:pb-10 sm:pt-10'>
-            <div className='mb-5 font-ranga text-2xl underline decoration-blue-400 decoration-4 underline-offset-4 sm:text-4xl'>
-              DAVID CANDO MUSIC
-            </div>
+        <div
+          className={`mx-auto flex w-screen flex-col items-center pb-4 pt-4 
+                      sm:w-[1000px]`}
+        >
+          <div
+            className={`flex w-full flex-col items-center justify-between bg-slate-100 pb-5 pt-5 
+                        sm:rounded-md sm:pb-10 sm:pt-10`}
+          >
+            <div className='title'>DAVID KANDO MUSIC</div>
             <div className='grid grid-cols-2 gap-8 sm:grid-cols-4 sm:gap-2'>
               {items}
             </div>
             <br />
           </div>
-          <div className='p-5 text-xl'>{children}</div>
+          <div className='w-full text-xl'>{children}</div>
         </div>
       </body>
     </html>
